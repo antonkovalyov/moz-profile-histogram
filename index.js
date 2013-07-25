dfeb207c15c50be799eca4b09887047fa01f3050
Anton Kovalyov <anton@kovalyov.net>
2013-07-24 17:33:03 -0700

    display symbol name on hover

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 
 "use strict";
 
-function px(num)    { return num + "px" }
+function px(num)    { return parseInt(num, 10) + "px" }
 function hex(str)   { return "#" + str }
+function rnd(num)   { return Math.round(num) }
 function mean(nums) { return nums.reduce(function (p, c) { return p + c }, 0) / nums.length }
-function maxn(nums)  { return nums.reduce(function (p, c) { return p > c ? p : c }, -1) }
+function maxn(nums) { return nums.reduce(function (p, c) { return p > c ? p : c }, -1) }
 
 function createCanvas(w, h) {
   var canvas = document.createElement("canvas");
@@ -28,7 +29,7 @@ function draw() {
     var ret = "";
 
     for (var i = 0; i < 6; i++) {
-      ret += ltr[Math.round(Math.random() * 15)];
+      ret += ltr[rnd(Math.random() * 15)];
     }
 
     return hex(ret);
@@ -39,6 +40,7 @@ function draw() {
   var canvas = createCanvas(width, height);
   var ctx    = canvas.getContext("2d");
   var max    = maxn(data.map(function (frames) { return frames.length }));
+  var blocks = [];
 
   document.querySelector("#charts").appendChild(canvas);
 
@@ -51,14 +53,42 @@ function draw() {
   set.forEach(function (frames) {
     h = height / max;
 
+    for (var xx = x; xx < x + 20; xx++) {
+      blocks[xx] = new Array(frames.length);
+    }
+
     frames.forEach(function (frame, i) {
-      var y = h * i;
+      var y = rnd(h * i);
 
       ctx.fillStyle = colors[frame];
       ctx.fillRect(x, y, 19, h - 1);
+
+      for (var xx = x; xx < x + 20; xx++) {
+        for (var yy = y; yy < y + 20; yy++) {
+          blocks[xx][yy] = frame;
+        }
+      }
     });
 
     x += 20;
+  });
+
+  var rect = canvas.getBoundingClientRect();
+  var div = document.createElement("div");
+
+  div.className = "symbol";
+  div.style.width = px(rect.width - 10);
+  div.style.left = px(rect.left);
+  div.style.top = px(rect.top);
+
+  document.querySelector("#charts").appendChild(div);
+
+  canvas.addEventListener("mousemove", function (ev) {
+    var x = rnd(ev.pageX - rect.left);
+    var y = rnd(rect.height - (ev.pageY - rect.top));
+    var sym = DATA.symbols[blocks[x][y]];
+
+    div.innerHTML = sym ? sym.symbolName : "";
   });
 
   lines();
@@ -71,7 +101,7 @@ function lines() {
 
   var width  = 900;
   var height = 50;
-  var step   = Math.floor(data.length / (width / 5));
+  var step   = rnd(data.length / (width / 5));
   var canvas = createCanvas(width, height);
   var ctx    = canvas.getContext("2d");
 
